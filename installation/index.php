7067d2255512915ba4d31b96a37c5476e1ee08ed
Mike Di Domenico <mike@mouseware.net>
2013-03-17 13:10:17 -0400

    Created install file

---
+++ installation/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * MseBase - PHP system to develop web applications
+ * @author Mike Di Domenico
+ * @copyright 2008 - 2013 Mike Di Domenico
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+define("Access", 1);
+
+echo "Installation not built yet... ";
+?>
